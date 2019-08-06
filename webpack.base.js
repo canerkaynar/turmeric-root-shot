23cad9aa463c946d5d708b21297d36ed2926e950
Caner Kaynar <canerkaynar@hotmail.com>
2019-08-07 00:59:14 +0300

    port changed

--- webpack.base.js
+++ webpack.base.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV) {
 const currentPath = path.join(__dirname);
 const basePath = currentPath + '/.env';
 const fileEnv = dotenv.config({ path: basePath }).parsed;
-envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+envKeys = fileEnv && Object.keys(fileEnv).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
   return prev;
 }, {});
