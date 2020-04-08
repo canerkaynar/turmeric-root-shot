c1df6864ed430df45ae6d075e0a4cb88cf404194
Caner Kaynar <canerkaynar@hotmail.com>
2020-04-08 15:34:45 +0300

    comment added

--- frontend/Routes.js
+++ frontend/Routes.js
@@ -1,21 +1,22 @@
 /*eslint-disable*/
-import React from 'react';
-import Root from './Root';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
+import React from "react";
+import Root from "./Root";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
+//Routes
 export default [
   {
     ...Root,
     routes: [
       {
         ...HomePage,
-        path: '/',
-        exact: true
+        path: "/",
+        exact: true,
       },
       {
-        ...NotFoundPage
-      }
-    ]
-  }
+        ...NotFoundPage,
+      },
+    ],
+  },
 ];
